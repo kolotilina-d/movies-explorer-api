0a427dfd28bf21db359ca173db630702e9824c41
Darya Kolotilina <colotilina.d@yandex.ru>
2023-09-13 14:04:22 +0200

    'port'

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const { limiter } = require('./middlewares/limiter');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
